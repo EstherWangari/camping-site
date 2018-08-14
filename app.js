dcd94bdca4f03a912cd42468157b9f2c11bf4291
EstherWangari <wangarie0@gmail.com>
2018-08-14 12:21:04 +0300

    installed body-parser to require the body and added a post route that adds a new object on my campground it redirects

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var express = require("express");
 var app = express();
-app.set("view engine","ejs");
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+var campgrounds = [
+  {name:"kare",title:"bad"},
+  {name:"mike",title:"good"}
+]
 
 app.get("/", function(req,res){
   res.render("landing")
 });
 app.get("/campgrounds",function(req,res){
-  var campgrounds = [
-    {name:"kare",title:"bad"},
-    {name:"mike",title:"good"}
-  ]
+
   res.render("campgrounds",{campgrounds:campgrounds});
+});
+app.post("/campgrounds",function(req,res){
+  var name = req.body.name;
+  var title = req.body.title;
+  var newCampGround = {name:name,title:title};
+  campgrounds.push(newCampGround);
+  res.redirect("/campgrounds");
+});
+app.get("/campgrounds/new",function(req,res){
+  res.render("new");
 });
 
 
