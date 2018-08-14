d822e55d539f785ab2cbd2aa2ba9589cd17deb15
EstherWangari <wangarie0@gmail.com>
2018-08-14 14:38:29 +0300

    added a navbar on campground.ejs and included it to the header.ejs so it can appear in all pages and styled it using bootstrap docs

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 var campgrounds = [
-  {name:"kare",title:"bad"},
-  {name:"mike",title:"good"}
+  {name:"kare",image:"https://pixabay.com/get/ea36b70928f21c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"mike",image:"https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f4c07ea0eeb4b1_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"},
+  {name:"lamar",image:"https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144291f1c67ea4ecbc_340.jpg"}
 ]
 
 app.get("/", function(req,res){
@@ -18,8 +25,8 @@ app.get("/campgrounds",function(req,res){
 });
 app.post("/campgrounds",function(req,res){
   var name = req.body.name;
-  var title = req.body.title;
-  var newCampGround = {name:name,title:title};
+  var image = req.body.image;
+  var newCampGround = {name:name,image:image};
   campgrounds.push(newCampGround);
   res.redirect("/campgrounds");
 });
